d52d79a8cb05dfe90f66ba6b689a67e1ec01411f
David <davidbarbosa425@gmail.com>
2022-10-30 17:12:25 -0300

    rotas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Router} from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
@@ -16,9 +17,10 @@ export class ListarPensamentoComponent implements OnInit {
   haMaisPensamentos: boolean = true;
   filtro : string = '';
   favorito: boolean = false;
-  listaFavoritos: Pensamento[] = []
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural'
 
-  constructor(private service: PensamentoService) { }
+  constructor(private service: PensamentoService, private router: Router) { }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual, this.filtro, this.favorito).subscribe((listaPensamentos)=>{
@@ -43,6 +45,8 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   listarFavoritos(){
+
+    this.titulo = 'Meus Favoritos'
     this.favorito = true
     this.haMaisPensamentos = true
     this.paginaAtual = 1
@@ -50,5 +54,14 @@ export class ListarPensamentoComponent implements OnInit {
       this.listaPensamentos = listaPensamentos
       this.listaFavoritos = listaPensamentos
     })
+  }
+
+  recarregarComponente() {
+
+    this.favorito = false
+    this.paginaAtual = 1
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
   }
 }
