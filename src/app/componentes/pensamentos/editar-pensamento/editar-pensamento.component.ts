128cba54f78ef2d16df9dbf5615b99e092dd5c59
David <davidbarbosa425@gmail.com>
2022-10-30 14:40:41 -0300

    refaturacao tela de edicao

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -18,28 +19,53 @@ export class EditarPensamentoComponent implements OnInit {
     favorito: false
   }
 
+  formulario!: FormGroup;
+
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
+
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento)=>{
       this.pensamento = pensamento
     })
+
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
+      modelo: ['modelo1'],
+      favorito: [false],
+      id: [id]
+    })
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
-    })
+      if(this.formulario.valid){
+      this.service.editar(this.formulario.value).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    } 
   }
   
   cancelar() {
     this.router.navigate(['/listarPensamento'])
 
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) return 'botao'
+    return 'botao__desabilitado'
   }
 
 }
